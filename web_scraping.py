76f4c6c832ae20786303627facb780776469db4f
Aishwarya <aishwaryaa1@hotmail.com>
2021-11-01 01:57:17 +0530

    added textin key value pair

--- web_scraping.py
+++ web_scraping.py
@@ -5,15 +5,18 @@ import re
 import pandas as pd
 queries=['ui figma', 'digital art']
 url_lst=[]
+text_lst=[]
 with requests.Session() as s:
     for query in queries:
         response=requests.get("https://www.behance.net/search/projects?tracking_source=typeahead_search_direct&search="+query)
         soup=bs(response.content,'lxml')
+        
         for a in soup.find_all('a',attrs={'href': re.compile("^https://")}):
-            url=a['href']
-            #print("Found the URL:", url)
-            url_lst.append(url)
-#print(url_lst)
-df=pd.DataFrame(url_lst, columns=['url'])
+            url = a['href']
+            txtt = a.text.strip()
+            #url_lst.append(url)
+            url_lst.append({"TEXT":txtt, "URLA":url})
+#df=pd.DataFrame(url_lst, columns=['url'])
+df = pd.DataFrame.from_dict(url_lst, orient='columns')
 df.to_csv('result.csv')
 print(df)
